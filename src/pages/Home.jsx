7ed4e7c32213683ae2ff8519c089b3acbdf9bab0
Francisco <francisco@pop-os.localdomain>
2022-11-07 23:23:49 -0600

    add: img and animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import PageContent from '../layouts/PageContent'
 import Title from '../layouts/Title'
+import './Home.css'
 
 const Home = () => {
   return (
     <PageContent>
       <Title />
-      <div></div>
+      <div className='box-mensaje'>
+        <h1 className='mensaje'>Crea tus intercambios secretos para Navidad</h1>
+      </div>
+      <div className='imgContainer'>
+        <img
+          className='img'
+          alt='gift image'
+          src='/public/Two_hands_with_gift.png'
+        />
+      </div>
     </PageContent>
   )
 }
