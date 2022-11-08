66d81b3dea013e3929fd308d31bfd93560b34d90
Francisco <francisco@pop-os.localdomain>
2022-11-08 12:16:20 -0600

    add: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
